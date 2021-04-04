11737e53344295697934c2222578fd2bb76c6a6b
unknown <allenreysarmiento@outlook.com>
2021-04-03 19:37:07 -0700

    Add initial setup and current user route

---
+++ src/routes/__test__/current-user.test.ts
@@ -0,0 +1,5 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it('responds with details about the current user', async () => {
+});
---
+++ src/test/setup.ts
@@ -0,0 +1,20 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import request from 'supertest';
+import { app } from '../app';
+
+let mongo: any;
+
+beforeAll(async () => {
+  process.env.JWT_KEY = 'asdfasdf';
+
+  mongo = new MongoMemoryServer();
+  const mongoUri = await mongo.getUri();
+
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+});
+
+beforeEach(async () => {});
